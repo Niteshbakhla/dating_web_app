ba940aa824b6943da521c5216aa7853658647852
Nitesh bakhla <niteshbakhla007@gmail.com>
2026-01-07 14:39:26 +0530

    refactor: cofigure the cookies

--- backend/src/utilis/cookieOption.js
+++ backend/src/utilis/cookieOption.js
@@ -2,11 +2,11 @@ import config from "../config/config.js";
 
 export const getAuthCookieOptions = () => {
             const isProd = config.NODE_ENV === "production";
-         
+
             return {
                         httpOnly: true,
                         secure: isProd,
-                        sameSite: isProd ? "strict" : "lax",
+                        sameSite: isProd ? "none" : "lax",
                         maxAge: 7 * 24 * 60 * 60 * 1000,
             };
 };
